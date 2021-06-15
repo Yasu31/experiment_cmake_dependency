0340a88a1f824a35bc217753bae89eefea6d2531
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-06-15 13:12:36 +0900

    minimal working example

---
+++ include/definition.h
@@ -0,0 +1,2 @@
+// when this is changed and make is run again, program is not updated!
+#define TEST 9
---
+++ source.cpp
@@ -0,0 +1,8 @@
+#include <header.h>
+#include DEFINITION_H
+#include <iostream>
+int main(){
+    std::cout << DEFINITION_H << std::endl;
+    std::cout << TEST << std::endl;
+    return 1;
+}
